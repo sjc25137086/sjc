66bafdf46699187de05b8b50d8def141e0ce4f57
sjc <891109354@qq.com>
2020-09-28 11:47:36 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/Login/login')
+  }
+]
+
+const VueRouterPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(to) {
+  return VueRouterPush.call(this, to).catch(err => err)
+}
+
+//replace
+const VueRouterReplace = VueRouter.prototype.replace
+VueRouter.prototype.replace = function replace(to) {
+  return VueRouterReplace.call(this, to).catch(err => err)
+}
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ vue.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  configureWebpack:{
+    // externals : {
+    //   'vue': 'Vue',
+    //   'vue-router': 'vueRouter',
+    //   'element-ui': 'element-ui',
+    // }
+  },
+  // productionSourceMap:false,
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  devServer: {
+    port: 3344,
+    open: true
+    // 设置代理，使客户端通过服务器的方式与服务器通信，解决跨域问题
+    // proxy: {}
+  },
+  // css相关的配置
+  css: {
+    loaderOptions: {
+      sass: {
+        // 这里data换成 prependData  并且重启vue项目即可
+        // 设置相对路径
+        prependData: `@import "@/assets/scss/_variable.scss";`
+      }
+    }
+  }
+  
+}
